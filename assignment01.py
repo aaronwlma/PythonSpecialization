98aa73b645473ff4540ca2bb37d90baf23426f75
Aaron Ma <44814805+aaronwlma@users.noreply.github.com>
2018-11-29 15:03:06 -0800

    Delete assignment01.py

--- assignment01.py
+++
@@ -1,7 +0,0 @@
-# Assignment 01 - Chapter 1
-# For University of Michigan's Coursera Specialization "Python for Everybody"
-# Course 1 - Programming for Everybody (Getting Started with Python)
-
-# Problem: Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
-
-print('hello world')
