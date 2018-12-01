8280f6df4396f63f4a581a4cacabdcc6e06c0335
Aaron Ma <44814805+aaronwlma@users.noreply.github.com>
2018-11-30 21:36:50 -0800

    Update assignment06.py

--- Course02_PythonDataStructures/assignment06.py
+++ Course02_PythonDataStructures/assignment06.py
@@ -1,13 +1,3 @@
-# Assignment 05a - Chapter 5
-# For University of Michigan's Coursera Specialization "Python for Everybody"
-# Course 1 - Programming for Everybody (Getting Started with Python)
-
-# Problem: 5.2 Write a program that repeatedly prompts a user for integer
-# numbers until the user enters 'done'. Once 'done' is entered, print out the
-# largest and smallest of the numbers. If the user enters anything other than
-# a valid number catch it with a try/except and put out an appropriate message
-# and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
-
 # Assignment 06 - Chapter 6
 # For University of Michigan's Coursera Specialization "Python for Everybody"
 # Course 2 - Python Data Structures
